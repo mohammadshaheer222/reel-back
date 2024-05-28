e57f6c21530c596ba39a69352434b275fd2a2724
mohammad shaheer <shaheercse2022@gmail.com>
2024-05-28 15:48:22 +0530

    add photos

--- App.js
+++ App.js
@@ -8,26 +8,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use((req, res, next) => {
-  console.log(`Request from origin: ${req.headers.origin}`);
-  next();
-});
 app.use(
   cors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        "http://localhost:5173",
-        "https://reelman-front.onrender.com",
-      ];
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: ["http://localhost:5173","https://reelman-front.onrender.com"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use("/uploads", express.static("uploads"));
@@ -50,7 +34,6 @@ app.use(
 );
 
 app.use("/api/v2/user", userContactForm);
-
 //for error handling
 app.use(ErrorHandler);
 app.use(notFound);
